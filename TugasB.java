c7fa22b0c3c4080a17b3e0920232c5b321deb54d
M.Ridho Perdana <90363557+Namarsel1sT@users.noreply.github.com>
2021-09-19 18:09:35 +0700

    Update TugasB.java

--- TugasB.java
+++ TugasB.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class tugasB {
     public static void main(String[] args) {
-        Scanner scan1 = new Scanner(System.in);
-        int a = scan1.nextInt();
-        int b = scan1.nextInt();
+        Scanner scanscan = new Scanner(System.in);
+        int a = scanscan.nextInt();
+        int b = scanscan.nextInt();
         System.out.println(a + b);
     }
 }
